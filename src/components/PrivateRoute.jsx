f367fdcdc37e20645ede40749b35feb064293925
Lucasss1997 <lucas.stephenson@icloud.com>
2025-08-11 23:17:59 +0100

    Create PrivateRoute.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { isLoggedIn } from '../lib/auth';
+
+export default function PrivateRoute() {
+  return isLoggedIn() ? <Outlet /> : <Navigate to="/login" replace />;
+}

37da2864e2d6e5134d36c0321e86960645aa0bd7
Lucasss1997 <lucas.stephenson@icloud.com>
2025-08-11 23:36:28 +0100

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to Pub Game</h1>
+      <p>Select a game or login to manage your pub’s games.</p>
+    </div>
+  );
+}
